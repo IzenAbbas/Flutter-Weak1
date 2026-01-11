215c01bdf91068f57c2cf9cac648b0c5b822cf2c
Izen Abbas <izen.khokhar@gmail.com>
2026-01-11 23:28:27 +0500

    first commit

---
+++ android/app/src/main/kotlin/com/example/weak1_tasks/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.weak1_tasks
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:weak1_tasks/task1.dart';
+import 'package:weak1_tasks/task2.dart';
+import 'package:weak1_tasks/task3.dart';
+import 'package:weak1_tasks/task4.dart';
+
+void main() {
+  //runApp(MaterialApp(title: 'Week1_Tasks', home: Task1()));
+  //runApp(MaterialApp(title: 'Week1_Tasks', home: Task2()));
+  //runApp(MaterialApp(title: 'Week1_Tasks', home: Task3()));
+  runApp(MaterialApp(title: 'Week1_Tasks', home: Task4()));
+}
